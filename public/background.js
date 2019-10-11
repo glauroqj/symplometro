809ce22140b899557acd923913be463edcecc808
glauroqj <glauro.juliani@hotmail.com>
2019-10-11 00:52:37 -0300

    add: bg template

--- public/background.js
+++ public/background.js
@@ -1,4 +1,26 @@
 (function() {
+  const credentials = {
+    apiKey: "AIzaSyAOq1-ibTMS4HChhuxpCeCwUFFQai16I1c",
+    authDomain: "symplometro.firebaseapp.com",
+    databaseURL: "https://symplometro.firebaseio.com",
+    projectId: "symplometro",
+    storageBucket: "symplometro.appspot.com",
+    messagingSenderId: "106786872116",
+    appId: "1:106786872116:web:9c7693352d516bb425e361"
+  }
+  firebase.initializeApp(credentials)
+  const db = firebase.firestore()
+
+  db.collection('events')
+    .doc('config')
+    .get()
+    .then(doc => {
+      console.log('< FIRESTORE : GET DATA > ', doc.data())
+    })
+    .catch(error => {
+      console.warn('< DATABASE : GET : ERROR > ', error )
+    })
+
   getEvents('init')
 
   setInterval(()=> {
@@ -17,34 +39,34 @@
     const notificationPayload = localStorage.getItem('Symplometro-Data')
     if (notificationPayload === null) localStorage.setItem('Symplometro-Data', JSON.stringify({notification: true}))
 
-    xhr.onload = () => {
-      // Process our return data
-      if (xhr.status >= 200 && xhr.status < 300) {
-        // What do when the request is successful
-        // console.log('success!', xhr)
-        const parser = new DOMParser()
-        const domResponse = parser.parseFromString(xhr.responseText, 'text/html')
-        // console.log('DOM RESPONSE: >>>> ', domResponse)
-        const countEvents = domResponse.querySelectorAll('h1 span strong')[0].innerText
-        // console.log('Events >>>>>>>> ', countEvents)
-        localStorage.setItem('Count-Event', countEvents)
+    // xhr.onload = () => {
+    //   // Process our return data
+    //   if (xhr.status >= 200 && xhr.status < 300) {
+    //     // What do when the request is successful
+    //     // console.log('success!', xhr)
+    //     const parser = new DOMParser()
+    //     const domResponse = parser.parseFromString(xhr.responseText, 'text/html')
+    //     // console.log('DOM RESPONSE: >>>> ', domResponse)
+    //     const countEvents = domResponse.querySelectorAll('h1 span strong')[0].innerText
+    //     // console.log('Events >>>>>>>> ', countEvents)
+    //     localStorage.setItem('Count-Event', countEvents)
 
-        const onlyNumbers = countEvents.split(' ')[0].split('.')[0]
+    //     const onlyNumbers = countEvents.split(' ')[0].split('.')[0]
 
-        chrome.browserAction.setBadgeText({text: `${onlyNumbers}K`})
+    //     chrome.browserAction.setBadgeText({text: `${onlyNumbers}K`})
 
-        if (action === 'refresh') {
-          showNotification(countEvents)
-        }
+    //     if (action === 'refresh') {
+    //       showNotification(countEvents)
+    //     }
 
-      } else {
-        // What do when the request fails
-        console.log('The request failed!')
-      }
-    }
+    //   } else {
+    //     // What do when the request fails
+    //     console.log('The request failed!')
+    //   }
+    // }
     
-    xhr.open('GET', 'https://www.sympla.com.br/index.html')
-    xhr.send()
+    // xhr.open('GET', 'https://www.sympla.com.br/index.html')
+    // xhr.send()
   }
 
   function showNotification(data) {
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
+// const id = btoa(new Date)
+
 const App = () => {
   const db = firebase.firestore()
   
