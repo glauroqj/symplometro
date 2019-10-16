e3761e7d80d61cc31c68e3ab90516f5e02015075
glauroqj <glauro.juliani@hotmail.com>
2019-10-15 21:27:35 -0300

    fix: timeout

--- public/background.js
+++ public/background.js
@@ -11,70 +11,6 @@
   firebase.initializeApp(credentials)
   const db = firebase.firestore()
   
-  function getEvents(action) {
-
-    const notificationPayload = localStorage.getItem('symplometro-data')
-    if (notificationPayload === null) localStorage.setItem('symplometro-data', JSON.stringify({notification: true}))
-
-    /** get events from database */
-    db.collection('events')
-    .doc('config')
-    .get()
-    .then(doc => {
-      // console.log('< FIRESTORE : GET DATA > ', doc.data())
-      const eventsPayload = doc.data()
-      /** show badge */
-      const onlyNumbers = doc.data().count.split(' ')[0].split('.')[0]
-      chrome.browserAction.setBadgeText({text: `${onlyNumbers}K`})
-      /** verify notification */
-      const userConfigs = localStorage.getItem('symplometro-user')
-      /** if doesnt exist user, check/create one */
-      if (userConfigs === null) checkUser()
-
-      if (userConfigs) {
-        /** check actions and user preferences */
-        if (action === 'refresh') showNotification(eventsPayload.count)
-      }
-    })
-    .catch(error => {
-      console.warn('< DATABASE : GET : ERROR > ', error )
-    })
-  }
-
-  function showNotification(data) {
-    const notificationPayload = localStorage.getItem('symplometro-data')
-
-    if (notificationPayload !== null) {
-      const notification = JSON.parse(notificationPayload).notification
-      if (!notification) return false
-    }
-
-    let options = {
-      type: 'basic',
-      iconUrl: './symplometro-128.png',
-      message: data,
-      title: 'Eventos Online',
-      eventTime: 3000
-    }
-      
-    // console.log('< SHOW NOTIFICATION > ', options)
-
-    if (Notification.permission === 'granted') {
-			// let notification = new Notification('', options);
-			chrome.notifications.create('', options)
-			return false
-		}
-
-    if (Notification.permission !== 'denied') {
-			Notification.requestPermission((permission) => {
-				if (permission === 'granted') {
-					chrome.notifications.create('', options)
-				}
-			})
-			return false
-		}
-  }
-
   function checkUser() {
     /** check user */
     let symplometroUser = localStorage.getItem('symplometro-user')
@@ -106,7 +42,7 @@
       /** save in database */
       db.collection('users')
       .add(payload)
-      .then(ref => {
+      .then(function(ref) {
         payload.id = ref.id
         // console.log('< USER SAVED IN DATABASE > ', payload)
         localStorage.setItem('symplometro-user', JSON.stringify(payload))
@@ -114,20 +50,85 @@
         window.userID = ref.id
         getEvents('init')
       })
-      .catch(error => {
+      .catch(function(error) {
         console.warn('< ERROR SAVE USER IN DATABASE > ', error)
         getEvents('init')
       })
     }
 
     /** start interval */
-    setInterval(()=> {
-      // console.log('< SET INTERVAL : TIMER > ', window.timeToNotification)
+    setInterval(function() {
+      // console.log('< SET INTERVAL : TIMER > ')
       getEvents('refresh')
-    }, 2.7e+6) /* 2.7e+6 = 45 minutes */
+    }, 2700000) /* 2.7e+6 = 45 minutes */
 
   }
 
+  function getEvents(action) {
+
+    const notificationPayload = localStorage.getItem('symplometro-data')
+    if (notificationPayload === null) localStorage.setItem('symplometro-data', JSON.stringify({notification: true}))
+
+    /** get events from database */
+    db.collection('events')
+    .doc('config')
+    .get()
+    .then(function(doc) {
+      // console.log('< FIRESTORE : GET DATA > ', doc.data())
+      const eventsPayload = doc.data()
+      /** show badge */
+      const onlyNumbers = doc.data().count.split(' ')[0].split('.')[0]
+      chrome.browserAction.setBadgeText({text: `${onlyNumbers}K`})
+      /** verify notification */
+      const userConfigs = localStorage.getItem('symplometro-user')
+      /** if doesnt exist user, check/create one */
+      if (userConfigs === null) checkUser()
+
+      if (userConfigs) {
+        /** check actions and user preferences */
+        if (action === 'refresh') showNotification(eventsPayload.count)
+      }
+    })
+    .catch(function(error) {
+      console.warn('< DATABASE : GET : ERROR > ', error )
+    })
+  }
+
+  function showNotification(data) {
+    const notificationPayload = localStorage.getItem('symplometro-data')
+
+    if (notificationPayload !== null) {
+      const notification = JSON.parse(notificationPayload).notification
+      if (!notification) return false
+    }
+
+    let options = {
+      type: 'basic',
+      iconUrl: './symplometro-128.png',
+      message: data,
+      title: 'Eventos Online',
+      eventTime: 3000
+    }
+      
+    // console.log('< SHOW NOTIFICATION > ', options)
+
+    if (Notification.permission === 'granted') {
+			// let notification = new Notification('', options);
+			chrome.notifications.create('', options)
+			return false
+		}
+
+    if (Notification.permission !== 'denied') {
+			Notification.requestPermission(function(permission) {
+				if (permission === 'granted') {
+					chrome.notifications.create('', options)
+				}
+			})
+			return false
+		}
+  }
+
+  /** init */
   checkUser()
 
 })()
