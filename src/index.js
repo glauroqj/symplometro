8bac6c9ebe0f8247242bd57c98048bca523d6d40
glauroqj <glauro.juliani@hotmail.com>
2019-10-16 23:07:08 -0300

    trying...

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 /** style */
 import './main.scss'
+/** core */
+import App from './App'
 /** firebase */
 import firebase from 'firebase/app'
 import {credentials} from './credentials'
-/** core */
-import App from './App'
+
+console.log('< ENV > ', process.env.NODE_ENV, secrets.CC_CREDENTIALS)
 
 firebase.initializeApp(credentials)
 
